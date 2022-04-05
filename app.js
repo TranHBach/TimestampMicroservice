9191ed808c94fc302fd1ce5175ace5f4a69c1242
TranHBach <ragoza0932447933@gmail.com>
2022-04-05 14:27:20 +0700

    First commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", "views");
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/api/:time", (req, res, next) => {
+    const params = req.params.time;
+    let timeUTC;
+    let timeUnix;
+    
+    if (+params == params) {
+        timeUTC = new Date(+params).toUTCString();
+        timeUnix = new Date(+params).getTime();
+    } else {
+        timeUTC = new Date(params).toUTCString();
+        timeUnix = new Date(params).getTime();
+    }
+    
+    const result = {
+        unix: timeUnix,
+        utc: timeUTC,
+    };
+    
+    if (timeUTC === "Invalid Date") {
+        return res.render("api", {
+            result: JSON.stringify({ error: "Invalid Date" }),
+        });
+    }
+    return res.render("api", { result: JSON.stringify(result) });
+});
+app.use("/api", (req, res, next) => {
+  return res.render("api", {
+    result: JSON.stringify({ unix: Date.now(), utc: new Date().toUTCString() }),
+  });
+});
+
+app.use("/", (req, res, next) => {
+  return res.render("index");
+});
+
+app.listen(3000);
