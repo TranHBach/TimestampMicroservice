452282e3fdce956898e85707095cf7cddad67b07
TranHBach <ragoza0932447933@gmail.com>
2022-04-14 18:36:15 +0700

    Return json object

--- app.js
+++ app.js
@@ -6,34 +6,32 @@ app.set("view engine", "ejs");
 app.set("views", "views");
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/api/:time", (req, res, next) => {
-    const params = req.params.time;
-    let timeUTC;
-    let timeUnix;
-    
-    if (+params == params) {
-        timeUTC = new Date(+params).toUTCString();
-        timeUnix = new Date(+params).getTime();
-    } else {
-        timeUTC = new Date(params).toUTCString();
-        timeUnix = new Date(params).getTime();
-    }
-    
-    const result = {
-        unix: timeUnix,
-        utc: timeUTC,
-    };
-    
-    if (timeUTC === "Invalid Date") {
-        return res.render("api", {
-            result: JSON.stringify({ error: "Invalid Date" }),
-        });
-    }
-    return res.render("api", { result: JSON.stringify(result) });
+  const params = req.params.time;
+  let timeUTC;
+  let timeUnix;
+
+  if (+params == params) {
+    timeUTC = new Date(+params).toUTCString();
+    timeUnix = new Date(+params).getTime();
+  } else {
+    timeUTC = new Date(params).toUTCString();
+    timeUnix = new Date(params).getTime();
+  }
+
+  const result = {
+    unix: timeUnix,
+    utc: timeUTC,
+  };
+
+  if (timeUTC === "Invalid Date") {
+    return res.render("api", {
+      result: JSON.stringify({ error: "Invalid Date" }),
+    });
+  }
+  return res.json(result);
 });
 app.use("/api", (req, res, next) => {
-  return res.render("api", {
-    result: JSON.stringify({ unix: Date.now(), utc: new Date().toUTCString() }),
-  });
+  return res.json({ unix: Date.now(), utc: new Date().toUTCString() });
 });
 
 app.use("/", (req, res, next) => {
